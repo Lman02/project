f69badb05acb80a56db82d16372c681dbf3ff3f5
Ilya <example@gmail.com>
2022-07-24 22:11:57 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5/5/67/3/1`);
+const user = "Ivan";
+alert (`Привет, ${user}`);
+
+console.log(5*2 === 10);
+
+const isChecked = true;
+      isClosed = false;
+      console.log(isChecked || isClosed);
