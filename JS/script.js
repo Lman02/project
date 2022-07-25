66b0e725f579ae65dfa13da040da489748b86b8d
Ilya <example@gmail.com>
2022-07-25 13:39:14 +0300

    first vers

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,18 @@
-const category = 'toys';
-console.log(`https://someurl.com/${category}/5/5/67/3/1`);
-const user = "Ivan";
-alert (`Привет, ${user}`);
+const numberOfFilm = +prompt ('Сколько фильмов вы уже посмотрели?', '');
 
-console.log(5*2 === 10);
+const personalMovieDB = {
+    count: numberOfFilm,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false     
+};
 
-const isChecked = true;
-      isClosed = false;
-      console.log(isChecked || isClosed);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
